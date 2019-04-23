e60405fd8eb1ffb8e4ebee5bfd5967a41c237cdb
Brent Kinney <48770271+brentkinney@users.noreply.github.com>
2019-04-22 20:32:36 -0700

    Add project files.

---
+++ CISC192Project4-AmazingDigitalClock/Source.cpp
@@ -0,0 +1,32 @@
+//This program takes in a given time from the user and determines if that time is normal or lucky.  The program also displays if there is any meaning behind the given time
+//written by Brent Kinney 4/25/19
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	char choice = 'y';
+	while (choice == 'y' || choice == 'Y')
+	{
+		string timeGiven;
+		cout << "Enter a time (HH:MM): ";
+		getline(cin, timeGiven);
+		cout << timeGiven << " is a normal time" << endl;
+
+
+		//see if the user wants to continue and continue if desired
+		cout << "Continue? (Y/N): ";
+		cin >> choice;
+		cout << endl;
+	}
+	//display author of the program
+	cout << "Programmed by Brent Kinney" << endl;
+	cout << "Press [Enter] key to end..."; //prompt lets user know
+	cin.ignore(256, '\n');
+	cin.get();
+
+	return 0;
+}
